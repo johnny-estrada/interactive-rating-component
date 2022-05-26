8d998e95046156515055a0baf14c9d27d6f97011
Johnny Estrada <johnnye562@yahoo.com>
2022-05-25 19:57:29 -0700

    Added button functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
-// 'use strict';
+'use strict';
+
+const form = document.querySelector('#ratings-form');
+const input = document.querySelector('#five');
+const message = document.querySelector('#msg');
+
+// Radio input
+
+// Button functionality
+form.addEventListener('submit', function(e) {
+	e.preventDefault();
+
+	const ratingInput = input.value; // 1
+	const newP = document.createElement('p'); // <p></p>
+
+	newP.classList.add('text');
+	newP.textContent = `You selected ${ratingInput} out of 5`;
+	message.appendChild(newP);
+});

1f660fb84b272ec9066c8edac535df20608124ed
Johnny Estrada <johnnye562@yahoo.com>
2022-06-07 15:20:13 -0700

    Added display none to click event listener

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,20 @@
 'use strict';
 
 const form = document.querySelector('#ratings-form');
+
 const input = document.querySelector('#five');
+
 const output = document.querySelector('#msg');
 
-// Radio input
-
 // Button functionality
-form.addEventListener('submit', function(e) {
-	e.preventDefault();
+form.addEventListener('submit', (f) => {
+	f.preventDefault();
 
 	const rating = input.value; // 1
 	const paragraph = document.createElement('p'); // <p></p>
+	const toggle = document.querySelector('.ratings');
+
+	toggle.style.display = 'none';
 
 	paragraph.classList.add('text');
 	paragraph.textContent = `You selected ${rating} out of 5`;

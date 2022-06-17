9b071f5a3f79680498b76a68ebd052dd3a9229af
Johnny Estrada <johnnye562@yahoo.com>
2022-06-17 00:42:34 -0700

    Created a function and moved the main code into submitForm

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,26 @@
 'use strict';
 
-const form = document.querySelector('.ratings-form');
-const ratingsCard = document.querySelector('.ratings');
-const button = document.querySelector('.btn-primary');
+function submitForm() {
+	const ratingsCard = document.querySelector('.ratings');
+	const form = document.querySelector('.ratings-form');
+	const button = document.querySelector('.btn-primary');
 
-const thankYouCard = document.querySelector('.thank-you');
-const message = document.querySelector('#msg');
+	const thankYouCard = document.querySelector('.thank-you');
+	const message = document.querySelector('#msg');
 
-form.addEventListener('submit', (event) => {
-	event.preventDefault();
+	form.addEventListener('submit', (event) => {
+		event.preventDefault();
 
-	const ratingValue = document.querySelector('input[name="rating"]:checked').value;
-	const paragraph = document.createElement('p'); // <p></p>
+		const rating = document.querySelector('input[name="rating"]:checked').value;
+		const paragraph = document.createElement('p'); // <p></p>
 
-	ratingsCard.style.display = 'none';
-	thankYouCard.style.display = 'flex';
+		paragraph.classList.add('text');
+		paragraph.textContent = `You selected ${rating} ouf of 5`;
+		message.appendChild(paragraph);
 
-	paragraph.classList.add('text');
-	paragraph.textContent = `You selected ${ratingValue} ouf of 5`;
-	message.appendChild(paragraph);
-});
+		ratingsCard.style.display = 'none';
+		thankYouCard.style.display = 'flex';
+	});
+}
+
+submitForm();
